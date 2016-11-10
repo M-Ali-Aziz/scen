2821c407eb188b20abdf1578f9c559c5fc7247f7
m.ali.aziz <m_ali1426@hotmail.com>
2016-11-10 08:30:21 +0100

    template file with style tag

--- public/sites/all/modules/custom/date_generator/templates/date_generator_template.tpl.php
+++ public/sites/all/modules/custom/date_generator/templates/date_generator_template.tpl.php
@@ -1,21 +1,42 @@
-<div class="before-events">
-  <div class="before-event-one">
-    <?php print render($before_nodes[0]); ?> 
-  </div>
-  <div class="before-event-two">
-    <?php print render($before_nodes[1]); ?>
-  </div>
-</div>
+<?php
+/**
+  * @file Email Template file for Date Generator module
+  */ 
+?>
 
-<div class="performance">
-  <?php print render($performance); ?>
-</div>
+<!-- Style -->
+<style>
+.main-mail-template-wrapper {
+  background: gray;
+}
+  
+</style><!-- Style end! -->
 
-<div class="after-events">
-  <div class="after-events-one">
-    <?php print render($after_nodes[0]); ?>
+
+
+<!-- Content -->
+<dir class="main-mail-template-wrapper">
+  <div class="before-events">
+    <div class="before-event-one">
+      <?php print render($before_nodes[0]); ?> 
+    </div>
+    <div class="before-event-two">
+      <?php print render($before_nodes[1]); ?>
+    </div>
   </div>
-  <div class="after-events-two">
-    <?php print render($after_nodes[1]); ?>
+
+  <div class="performance">
+    <?php print render($performance); ?>
   </div>
-</div>
+
+  <div class="after-events">
+    <div class="after-events-one">
+      <?php print render($after_nodes[0]); ?>
+    </div>
+    <div class="after-events-two">
+      <?php print render($after_nodes[1]); ?>
+    </div>
+  </div>
+</dir>
+
+
