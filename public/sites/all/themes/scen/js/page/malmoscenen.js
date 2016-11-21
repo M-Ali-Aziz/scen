2508acd25359b029dd97bc98238cee0491343ee1
m.ali.aziz <m_ali1426@hotmail.com>
2016-11-21 15:33:21 +0100

    Edited form submit btn styling, and added js efects

--- public/sites/all/themes/scen/js/page/malmoscenen.js
+++ public/sites/all/themes/scen/js/page/malmoscenen.js
@@ -3,9 +3,33 @@
   console.log('malmoscenen.js is ON!');
 
   /*
+   * Content-top
+   */
+  // Validate tha value of email(input)
+  // to efect the styling of the submit button's text color.
+  $(document).bind('input propertychange', function() {
+    var emailValue = $('#edit-email').val();
+      // console.log('Email value2= ' + emailValue:contains('@'));
+    if (emailValue &&
+        emailValue.indexOf('@') > 0 &&
+        emailValue.slice(-1) != '@' &&
+        emailValue.includes('.') &&
+        emailValue.slice(-1) != '.' &&
+        emailValue.length >= 5)
+    {
+      $('.form-submit').css('color', '#FFF');
+      $('.form-submit').css('box-shadow', '5px 5px rgba(0, 0, 0, 0.2)');
+    }
+    else{
+      $('.form-submit').css('color', 'rgba(255,255,255,.2)');
+      $('.form-submit').css('box-shadow', 'none');
+    }
+  });
+
+  /*
    * Footer
-  */
-  $(document).on('click', '.about-malmoscenen-button', function (){
+   */
+  $(document).on('click', '.about-malmoscenen-button', function(){
     $('.about-malmoscenen-underlay').css('display', 'block');
     $('.about-malmoscenen').css('display', 'block');
   });
