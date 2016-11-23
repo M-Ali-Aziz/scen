a1cf375efe1be3294c725a9375f52026e2bef82e
m.ali.aziz <m_ali1426@hotmail.com>
2016-11-23 11:13:12 +0100

    Created date generator result template

---
+++ public/sites/all/themes/scen/templates/page/content/date_generator_result_template.tpl.php
@@ -0,0 +1,26 @@
+<?php
+/**
+  * @file Template file from Date Generator module
+  * contain the result of the Date Generator form after submit
+  */
+?>
+
+<?php
+/**
+  * Note!!!
+  * Some of the classes are from the theme templates.
+  */
+?>
+
+
+
+<?php print render($before_nodes[0]); ?>
+
+
+<?php print render($before_nodes[1]); ?>
+
+
+<?php print render($performance); ?>
+
+
+<?php print render($after_node); ?> 
