e804d16e0d608a3681ad4738846a1473940c2bb3
m.ali.aziz <m_ali1426@hotmail.com>
2016-11-29 14:46:24 +0100

    About Malmöscen page as a node + styling

---
+++ public/sites/all/themes/scen/templates/page/ds-1col--node--24.tpl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file
+ * Default theme implementation that displays a node using the DS 1 column
+ * layout.
+ *
+ * If you'd like to create a different template file for nodes, you should
+ * create a new DS layout, and place it in the templates/ds folder, just like
+ * this implementation.
+ *
+ * Have a look at the example_layout folder in the DS folder for guidance.
+ *
+ * In order to print contextual links for this entity, there are two things
+ * are required:
+ * - Add the contextual-links-region class to the wrapper where the links
+ *   should appear.
+ * - Render the $contextual_links variable within this wrapper.
+ *
+ * @see oddbaby_preprocess()
+ */
+?>
+<div class="about-malmoscenen-underlay"></div>
+    <div class="about-malmoscenen">
+    <div class="about-malmoscenen-close-button"></div>
+    <div class="<?php print $classes; ?> <?php print $ds_content_classes; ?> contextual-links-region">
+      <?php print render($contextual_links); ?>
+      <?php print $ds_content; ?>
+    </div>
+</div>
+
